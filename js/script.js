7827a9c258a91fc44c06c02f082fc48c37fda4b5
kminus <kminus13@gmail.com>
2018-06-16 00:14:32 +0200

    V0.9

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
+// Preloader
+
+(function() {
+    var preloader = document.querySelector("#preloader");
+
+    window.onload = function () {
+        preloader.classList.add("fadeOut");
+        preloader.addEventListener("animationend", function() {
+            preloader.classList.add("d-none");
+        });
+    };
+
+})();
+
+// PARALLAX
+
 (function() {
 
     var scrolled,
         parallax,
         el = document.querySelectorAll('.parallax-animate');
 
-    window.addEventListener("scroll", function(e) {
+    window.addEventListener("scroll", function() {
         scrolled = this.pageYOffset;
 
         if (scrolled < 900) {
@@ -18,21 +34,49 @@
 
 })();
 
+// NAVIGATION MENU
+
 (function() {
 
     var btn = document.querySelector(".nav-btn"),
         navbar = document.querySelector("#navbar"),
-        cls = "nav-btn-active";
+        clsBtn = "nav-btn-active",
+        dNone = "d-none",
+        clsIn = "fadeInLeft",
+        clsOut = "fadeOutLeft",
+        leverIn = true;
+        leverOut = false;
 
-    btn.addEventListener("click", function() {
-        this.classList.toggle(cls);
-
-        if (this.classList.contains(cls)) {
-            navbar.classList.remove("d-none")
-        } else {
-            navbar.classList.add("d-none")
+    navbar.addEventListener("animationend", function() {
+        if(this.classList.contains(clsIn)) {
+            this.classList.remove(clsIn);
+            leverOut = true;
+        } else if(this.classList.contains(clsOut)) {
+            this.classList.remove(clsOut);
+            this.classList.add(dNone);
+            leverIn = true;
         }
-
-    })
+    }); 
     
+    btn.addEventListener("click", function() {
+        if(leverIn) {
+            leverIn = false;
+            leverOut = false
+            btn.classList.add(clsBtn);
+            navbar.classList.remove(dNone)
+            navbar.classList.add(clsIn);
+        } else if(leverOut) {
+            leverOut = false;
+            leverIn = false;
+            btn.classList.remove(clsBtn);
+            navbar.classList.add(clsOut);
+        }
+    });
+ 
+})();
+
+// SCROLL REVEAL
+
+(function() {
+  
 })();
